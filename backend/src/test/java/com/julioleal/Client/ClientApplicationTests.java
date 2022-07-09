eafa9da3c40e56fc9b360c4116e3370144069255
Julio Cesar Freitas Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2022-07-09 12:17:43 -0300

    Create project

---
+++ backend/src/test/java/com/julioleal/Client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julioleal.Client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
